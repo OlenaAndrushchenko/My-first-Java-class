dd5e63b1eac345bf5951cefdd2b247bef983d840
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-06-30 23:20:34 +0200

    format document

--- src/main/java/dev/olena/ex_person/models/Person.java
+++ src/main/java/dev/olena/ex_person/models/Person.java
@@ -16,7 +16,7 @@ public class Person {
     private int yearOfBirth;
     private String countryOfBirth;
     private char gender;
-    
+
     public Person(String name, String lastName, String id, int yearOfBirth, String countryOfBirth, char gender) {
         this.name = name;
         this.lastName = lastName;
@@ -29,43 +29,43 @@ public class Person {
     public String getName() {
         return name;
     }
-    
+
     public String getLastName() {
         return lastName;
     }
-    
+
     public String getId() {
         return id;
     }
-    
+
     public int getYearOfBirth() {
         return yearOfBirth;
     }
-    
+
     public String getCountryOfBirth() {
         return countryOfBirth;
     }
-    
+
     public char getGender() {
         return gender;
     }
-    
-    
+
     public void print() {
         System.out.println(this.toString());
     }
-    
+
     @Override
     public String toString() {
-        return "Person name: " + name + 
-        ", last name: " + lastName + 
-        ", DNI: " + id + 
-        ", year of birth: " + yearOfBirth +
-        ", country of birth: " + countryOfBirth +
-        ", gender: " + gender;
+        return "Person name: " + name +
+                ", last name: " + lastName +
+                ", DNI: " + id +
+                ", year of birth: " + yearOfBirth +
+                ", country of birth: " + countryOfBirth +
+                ", gender: " + gender;
     }
-    
 }
+
 // public void print() {
-//     System.out.println("Person name: " + name + ", last name: " + lastName + ", DNI: " + id + ", year of birth: " + yearOfBirth );
+// System.out.println("Person name: " + name + ", last name: " + lastName + ",
+// DNI: " + id + ", year of birth: " + yearOfBirth );
 // }
